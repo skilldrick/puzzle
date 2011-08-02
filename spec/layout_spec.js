4fe5ef51cbb886be492299b941235f65fae1add2
Skilldrick <skilldrick@gmail.com>
2011-08-02 18:15:54 +0100

    Add collision detection.

--- spec/layout_spec.js
+++ spec/layout_spec.js
@@ -1,2 +1,37 @@
-describe('Layout', function () {
+describe('layOutDay', function () {
+  it('requires an array of events', function () {
+    expect(function () {
+      layOutDay();
+    }).toThrow('Events must be an array');
+  });
+});
+
+describe('collision', function () {
+  it('requires two event objects', function () {
+    expect(function () {
+      collision();
+    }).toThrow('Both arguments must be events');
+
+    expect(function () {
+      collision({}, {});
+    }).toThrow('Both arguments must be events');
+
+    expect(function () {
+      collision({ start: 100, end: 200 }, { start: 210, end: 250 });
+    }).not.toThrow();
+  });
+
+  it('returns true if events collide', function () {
+    var ev1 = { start: 0, end: 100 };
+    var ev2 = { start: 50, end: 150 };
+    expect(collision(ev1, ev2)).toBeTruthy();
+    expect(collision(ev2, ev1)).toBeTruthy();
+  });
+
+  it('returns false if first event end and second start are same', function () {
+    var ev1 = { start: 0, end: 50 };
+    var ev2 = { start: 50, end: 200 };
+    expect(collision(ev1, ev2)).toBeFalsy();
+    expect(collision(ev2, ev1)).toBeFalsy();
+  });
 });
--- src/layout.js
+++ src/layout.js
@@ -2,17 +2,37 @@
 Lays out events for a single  day
  
 @param array  events  
- An array of event objects. Each event object consists of a start time, end
- Time (measured in minutes) from 9am, as well as a unique id. The
- Start and end time of each event will be [0, 720]. The start time will
- Be less than the end time.  The array is not sorted.
+An array of event objects. Each event object consists of a start time,
+end time (measured in minutes) from 9am, as well as a unique id. The
+start and end time of each event will be [0, 720]. The start time will
+be less than the end time.  The array is not sorted.
  
- @return array
- An array of event objects that has the width, the left and top positions set,
- In addition to start time, end time, and id. 
+@return array
+An array of event objects that has the width, the left and top positions
+set, In addition to start time, end time, and id. 
  
 **/
 
 function layOutDay(events) {
+  if (!_.isArray(events)) {
+    throw new TypeError('Events must be an array');
+  }
+}
 
+/**
+Helper functions
+**/
+
+function collision(event1, event2) {
+  if (!isEvent(event1) || !isEvent(event2)) {
+    throw new TypeError('Both arguments must be events');
+  }
+
+  return event1.end > event2.start && event1.start < event2.end
+}
+
+function isEvent(event) {
+  return event &&
+    (typeof event.start == 'number') &&
+    (typeof event.end == 'number');
 }
