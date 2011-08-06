348bd82590b1870e7fa50031ad9801ba68ca68f7
Skilldrick <skilldrick@gmail.com>
2011-08-06 13:46:34 +0100

    Make an event object, add space collision detection, and render function.

--- spec/layout_spec.js
+++ spec/layout_spec.js
@@ -10,39 +10,59 @@ describe('layOutDay', function () {
       {id: 0, start: 10, end: 40},
       {id: 1, start: 20, end: 50},
       {id: 2, start: 30, end: 50},
-      {id: 3, start: 40, end: 70}
+      {id: 3, start: 40, end: 70},
+      {id: 4, start: 60, end: 80}
     ];
     layOutDay(events);
   });
 });
 
-describe('collision', function () {
+describe('spaceCollision', function () {
+  it('returns false if events only collide by time', function () {
+    var ev1 = EventMaker({ start: 0, end: 100});
+    ev1.widthFactor = 2;
+    var ev2 = EventMaker({ start: 50, end: 150});
+    ev2.order = 1;
+    ev2.widthFactor = 2;
+    expect(spaceCollision(ev1, ev2)).toBeFalsy();
+  });
+
+  it('returns true if events collide by space', function () {
+    var ev1 = EventMaker({ start: 0, end: 100});
+    var ev2 = EventMaker({ start: 50, end: 150});
+    ev2.order = 1;
+    ev2.widthFactor = 2;
+    expect(spaceCollision(ev1, ev2)).toBeTruthy();
+  });
+});
+
+describe('timeCollision', function () {
   it('requires two event objects', function () {
     expect(function () {
-      collision();
+      timeCollision();
     }).toThrow('Both arguments must be events');
 
     expect(function () {
-      collision({}, {});
+      timeCollision({}, {});
     }).toThrow('Both arguments must be events');
 
     expect(function () {
-      collision({ start: 100, end: 200 }, { start: 210, end: 250 });
+      timeCollision({ start: 100, end: 200 }, { start: 210, end: 250 });
     }).not.toThrow();
   });
 
   it('returns true if events collide', function () {
     var ev1 = { start: 0, end: 100 };
     var ev2 = { start: 50, end: 150 };
-    expect(collision(ev1, ev2)).toBeTruthy();
-    expect(collision(ev2, ev1)).toBeTruthy();
+    expect(timeCollision(ev1, ev2)).toBeTruthy();
+    expect(timeCollision(ev2, ev1)).toBeTruthy();
   });
 
   it('returns false if first event end and second start are same', function () {
     var ev1 = { start: 0, end: 50 };
     var ev2 = { start: 50, end: 200 };
-    expect(collision(ev1, ev2)).toBeFalsy();
-    expect(collision(ev2, ev1)).toBeFalsy();
+    expect(timeCollision(ev1, ev2)).toBeFalsy();
+    expect(timeCollision(ev2, ev1)).toBeFalsy();
   });
 });
 
--- src/layout.js
+++ src/layout.js
@@ -13,28 +13,35 @@ set, In addition to start time, end time, and id.
  
 **/
 
-function layOutDay(events) {
-  if (!_.isArray(events)) {
+function layOutDay(rawEvents) {
+  if (!_.isArray(rawEvents)) {
     throw new TypeError('Events must be an array');
   }
-  events = sortByStartAndEnd(events);
+  rawEvents = sortByStartAndEnd(rawEvents);
 
-  //initialise each event's collidesWith array
-  _.each(events, function (event) {
-    event.collidesWith = [];
+  // create event objects
+  var events = _.map(rawEvents, function (event) {
+    return EventMaker(event);
   });
 
-  var organised = [];
+  //Tell each event which earlier events it collides with
   _.each(events, function (event, idx) {
-    event.widthDivisor = 1;
-    var prevEvents = _.first(events, idx);
+    var prevEvents = _.first(events, idx); //first n events
+
     _.each(prevEvents, function (prevEvent) {
-      if (collision(prevEvent, event)) {
-        prevEvent.collidesWith.push(event);
+      if (timeCollision(prevEvent, event)) {
         event.collidesWith.push(prevEvent);
       }
     });
   });
+
+  _.each(events, function (event, idx) {
+    if (idx === 0) {
+      return;
+    }
+  });
+
+
   console.log(events);
 
   //
@@ -52,16 +59,34 @@ function layOutDay(events) {
   //
 }
 
+function render(events) {
+  $calendar = $('#calendar');
+  $ev1 = $('<div class="event" />');
+  $ev1.css('height', 100);
+  $ev2 = $('<div class="event" />');
+  $ev2.css('height', 75);
+  $calendar.append($ev1);
+  $calendar.append($ev2);
+}
+
 /**
 Helper functions
 **/
 
-function collision(event1, event2) {
+function timeCollision(event1, event2) {
   if (!isEvent(event1) || !isEvent(event2)) {
     throw new TypeError('Both arguments must be events');
   }
 
   return event1.end > event2.start && event1.start < event2.end
+}
+
+function spaceCollision(event1, event2) {
+  if (!timeCollision(event1, event2)) {
+    return false;
+  }
+
+  return event1.right() > event2.left() && event1.left() < event2.right();
 }
 
 function isEvent(event) {
@@ -89,4 +114,23 @@ function sortByStartAndEnd(events) {
       return 0;
     }
   });
+}
+
+function EventMaker(rawEvent) {
+  return {
+    start: rawEvent.start,
+    end: rawEvent.end,
+    widthFactor: 1,
+    width: function () {
+      return Math.floor(600 * (1 / this.widthFactor));
+    },
+    order: 0, //horizontal ordinal position
+    left: function () {
+      return this.width() * this.order;
+    },
+    right: function () {
+      return this.left() + this.width();
+    },
+    collidesWith: []
+  };
 }
